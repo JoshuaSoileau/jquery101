f9f7ff8a77303e1919dec3b30b8fae548a73d81a
Joshua Soileau <joshua.soileau@gmail.com>
2014-06-08 22:01:04 -0400

    Add SASS files, Restructure Header Links, New 'Day' Files, Remove Unused Assets, Add Course Materials in 'Exercises' folder

--- javascripts/site.js
+++ javascripts/site.js
@@ -15,25 +15,19 @@ $.deck('.slide');
 
 var url = window.location.href;
 
-if ( url.indexOf('screencasts') > -1 ) {
-	$('.video_link').click(function(){
-		var data_src = $(this).data('src');
-		$('.active_video').removeClass('active_video');
-		$(this).addClass('active_video');
-		$('.video_iframe').attr('src', data_src);
-	});
-}
-
 
 // =================================
 // updates assignment on link clicks
 // =================================
 
-if ( url.indexOf('assignments') > -1 ) {
+if ( url.indexOf('day') > -1 ) {
+
 	$('.assignment').click(function(){
+		
 		$('.active_assignment_link').removeClass('active_assignment_link');
 		$(this).addClass('active_assignment_link');
 		link_index = $(this).data('index');
+
 		$('.assignment_text').each(function(){
 			var content_index = $(this).data('index');
 			if(content_index == link_index) {
@@ -41,5 +35,7 @@ if ( url.indexOf('assignments') > -1 ) {
 				$(this).addClass('active_assignment');
 			}
 		});
+
 	});
+
 }
